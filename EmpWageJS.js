9ec6f06340f234a238d74a9c88eda52b2be6d242
Shruti Verma <shrutiverma3105@gmail.com>
2021-05-24 20:27:32 +0530

    commit UC3

--- EmpWageJS.js
+++ EmpWageJS.js
@@ -1,5 +1,5 @@
-// //UC1
-// const IS_ABSENT = 0
+//UC1
+const IS_ABSENT = 0;
 
 // let empCheck = Math.floor(Math.random()*10)%2;
 // if(empCheck == IS_ABSENT){
@@ -10,24 +10,40 @@
 // }
 
 //UC2
-const IS_PART_TIME=1
-const IS_FULL_TIME=2
-const PART_TIME_HOURS=4
-const FULL_TIME_HOURS=8
-const WAGE_PER_HOUR=20
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
-let empHrs=0
-empCheck=Math.floor(Math.random()*10)%3
-switch(empCheck){
+let empHrs = 0;
+// empCheck=Math.floor(Math.random()*10)%3
+// switch(empCheck){
+//     case IS_PART_TIME:
+//         empHrs = PART_TIME_HOURS;
+//         break;
+//     case IS_FULL_TIME:
+//         empHrs = FULL_TIME_HOURS;
+//         break;
+//     default:
+//         empHrs = 0;
+// }
+
+// let empWage = empHrs * WAGE_PER_HOUR;
+// console.log("Emp Wage: "+empWage);
+
+//UC3
+function getWorkingHours(empCheck) {
+  switch (empCheck) {
     case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
+      return PART_TIME_HOURS;
     case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
+      return FULL_TIME_HOURS;
     default:
-        empHrs = 0;
+      return 0;
+  }
 }
-
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage: "+empWage);
+console.log("Emp Wage: " + empWage);
